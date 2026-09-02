07bceac74772afbbee17d10247b8b4f0b7911b39
AndrewShadowStar <andreweconomicsandfinance@gmail.com>
2026-09-02 15:06:14 +0300

    Update data.js

--- data.js
+++ data.js
@@ -38,34 +38,52 @@ const scheduleData = {
 };
 const locations = {
     "main": {
-        address: "Головний корпус, пр. Свободи, 18",
+        address: "Економічний факультет, пр. Свободи, 18",
         mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2573.076540306385!2d24.0255!3d49.8415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDnCsDUwJzI5LjQiTiAyNMKwMDEnMzEuOCJF!5e0!3m2!1suk!2sua!4v1690000000000",
-        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lviv_University.jpg/800px-Lviv_University.jpg"
+        photo: "Економічний факультет Львівського національного університету імені Івана Франка.webp"
     },
     "chornovola": {
-        address: "Ч - Чорновола 61",
+        address: "Університет банківської справи, Чорновола 61",
         mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2572.645003666666!2d24.0210!3d49.8490!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDnCsDUwJzU2LjQiTiAyNMKwMDEnMTUuNiJF!5e0!3m2!1suk!2sua!4v1690000000000",
-        photo: "https://tvoemisto.tv/media/gallery/full/l/n/lnu1_3.jpg"
+        photo: "Львівський інститут банківської справи.jpg"
     }
 };
 
 const teachersInfo = {
+    "int-sys": {
+        name: "Інтелектуальні системи аналізу даних у менеджменті",
+        type: "Екзамен",
+        teacherName: "Прийма Світлана Степанівна",
+        photo: "Прийма Світлана Степанівна.jpg"
+    },
+    "upr-zmin": {
+        name: "Управління змінами",
+        type: "Екзамен",
+        teacherName: "Грищук Анна Михайлівна",
+        photo: "Грищук Анна Михайлівна.jpg"
+    },
     "cyf-sys": {
         name: "Цифрові системи управління бізнесом",
         type: "Екзамен",
-        teacherName: "Мельник Богдан Костянтинович",
-        photo: "https://via.placeholder.com/300x400?text=Фото+Викладача" // Тимчасове фото
+        teacherName: "Мельник Богдан Кирилович",
+        photo: "Мельник Богдан Кирилович.jpg"
     },
     "tech-pid": {
         name: "Технології підтримки прийняття управлінських рішень",
         type: "Залік",
-        teacherName: "Мельник Богдан Костянтинович",
-        photo: "https://via.placeholder.com/300x400?text=Фото+Викладача"
+        teacherName: "Мельник Богдан Кирилович",
+        photo: "Мельник Богдан Кирилович.jpg"
     },
     "strat-upr": {
         name: "Стратегічне управління людськими ресурсами",
         type: "Екзамен",
         teacherName: "Приймак Василь Іванович",
-        photo: "https://via.placeholder.com/300x400?text=Фото+Викладача"
+        photo: "Приймак Василь Іванович.jpg"
+    },
+    "proj-men": {
+        name: "Проектний менеджмент",
+        type: "Екзамен",
+        teacherName: "Ноздріна Лариса Василівна",
+        photo: "Ноздріна Лариса Василівна.jpg"
     }
 };
