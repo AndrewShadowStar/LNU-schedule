4fc4bc508d00805d248234beddf41d8e4f317752
AndrewShadowStar <andreweconomicsandfinance@gmail.com>
2026-09-02 13:53:51 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,38 @@
+const scheduleData = {
+    wednesday: {
+        title: "СЕРЕДА",
+        classes: [
+            {
+                number: "6",
+                time: "16:40-18:00",
+                name: "Цифрові системи управління бізнесом",
+                teacher: "доц. Мельник Б. К.",
+                type: "lab",
+                room: "ауд.200",
+                infoId: "cyf-sys",
+                locId: "chornovola"
+            },
+            {
+                number: "7",
+                time: "18:10-19:30",
+                name: "Технології підтримки прийняття управлінських рішень",
+                teacher: "доц. Мельник Б. К.",
+                type: "lab",
+                room: "ауд.200",
+                infoId: "tech-pid",
+                locId: "chornovola"
+            },
+            {
+                number: "8",
+                time: "19:40-21:00",
+                name: "Стратегічне управління людськими ресурсами",
+                teacher: "проф. Приймак В. І.",
+                type: "lec",
+                room: "ауд.108",
+                infoId: "strat-upr",
+                locId: "main"
+            }
+        ]
+    }
+    // Сюди пізніше додамо monday, tuesday, thursday
+};
